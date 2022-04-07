b4d832830cb71ae5cba2a4f07e355b47e2db911f
Ivan Morozov <i.morozov@qsoft.ru>
2022-04-08 00:20:36 +0300

    first commit

--- JoyReactor/ContentView.swift
+++ JoyReactor/ContentView.swift
@@ -6,13 +6,71 @@
 //
 
 import SwiftUI
+import SwiftSoup
+import Kingfisher
 
 struct ContentView: View {
+    
+    @State var srcsStringArray: [String?] = []
+        
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ScrollView {
+            ForEach(srcsStringArray, id: \.self) { string in
+                if let string = string{
+                    if let url = URL(string: string){
+                        KFImage(url)
+                            .resizable()
+                            .aspectRatio(contentMode: .fill)
+                    }
+                }
+            }
+        }
+        .onAppear {
+            let url = URL(string: "http://joyreactor.cc")!
+            let session = URLSession.shared
+            let urlRequest = URLRequest(url: url)
+
+            let task = session.dataTask(with: urlRequest) { data, resp, error in
+                if let data = data {
+                    if let formattedData = String(data: data, encoding: .utf8) {
+                        
+                        do {
+                            let html: String = formattedData
+                            let doc: Document = try SwiftSoup.parse(html)
+                            let srcs: Elements = try doc.select("img[src]")
+                            self.srcsStringArray = srcs.array().map { el in
+
+                                if let url = try? el.attr("src").description{
+                                    if url.contains("post"){
+                                    print(url)
+                                    return url
+                                    }
+                                }
+                                return nil
+                            }
+                            
+            //                print(srcsStringArray)
+                            // do something with srcsStringArray
+                            
+                        } catch Exception.Error(let type, let message) {
+                            print(message)
+                        } catch {
+                            print("error")
+                        }
+                        
+                    }
+                }
+                if let error = error{
+                    print(error)
+                }
+            }
+
+            task.resume()
+        }
     }
+    
 }
+
 
 //struct ContentView_Previews: PreviewProvider {
 //    static var previews: some View {

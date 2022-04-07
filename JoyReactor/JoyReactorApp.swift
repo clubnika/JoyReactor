acc4520400467399be141cd7ddd2eff96cba70be
Ivan Morozov <i.morozov@qsoft.ru>
2022-04-08 00:10:54 +0300

    Initial Commit

---
+++ JoyReactor/JoyReactorApp.swift
@@ -0,0 +1,17 @@
+//
+//  JoyReactorApp.swift
+//  JoyReactor
+//
+//  Created by Морозов Иван on 08.04.2022.
+//
+
+import SwiftUI
+
+@main
+struct JoyReactorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
